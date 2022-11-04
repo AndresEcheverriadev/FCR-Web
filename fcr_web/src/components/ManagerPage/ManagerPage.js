fec4fc11c76a1c1a56c25ad80d2dd3c08fea5f0e
Andres Echeverria <andres@brandpro.cl>
2022-11-04 20:54:17 -0300

    modal add obituario

--- fcr_web/src/components/ManagerPage/ManagerPage.js
+++ fcr_web/src/components/ManagerPage/ManagerPage.js
@@ -41,14 +41,122 @@ function ManagerPage() {
   );
 
   return (
-    <div className="mnaagerPageMainWrapper">
+    <div className="mnanagerPageMainWrapper">
       <header className="managerHeaderContainer">
         <Navbar />
       </header>
+
+      <div
+        class="modal fade"
+        id="modalAgregarObituario"
+        tabindex="-1"
+        aria-labelledby="exampleModalLabel"
+        aria-hidden="true"
+      >
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h1 class="modal-title fs-5" id="exampleModalLabel">
+                Agregar al obituario
+              </h1>
+              <button
+                type="button"
+                class="btn-close"
+                data-bs-dismiss="modal"
+                aria-label="Close"
+              ></button>
+            </div>
+            <div class="modal-body">
+              <form action="" className="formContainer">
+                <h5>Datos del fallecido</h5>
+                <div className="inputContainer">
+                  <label htmlFor="date">Fecha del fallecimiento:</label>
+                  <input type="date" name="date" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="nombreDeceso">Nombre del fallecido:</label>
+                  <input type="text" name="nombreDeceso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="segundoNombreDeceso">
+                    Segundo Nombre del fallecido:
+                  </label>
+                  <input type="text" name="segundoNombreDeceso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="paternoDeceso">Apellido paterno:</label>
+                  <input type="text" name="paternoDeceso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="maternoDeceso">Apellido materno:</label>
+                  <input type="text" name="maternoDeceso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="imgDeceso">Imagen obituario:</label>
+                  <input
+                    type="file"
+                    name="imgDeceso"
+                    accept="image/png, image/jpeg"
+                  />
+                </div>
+                <h5>Datos del funeral</h5>
+                <div className="inputContainer">
+                  <label htmlFor="lugarVelatorio">lugar velatorio:</label>
+                  <input type="text" name="lugarVelatorio" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="lugarResponso">lugar responso:</label>
+                  <input type="text" name="lugarResponso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="fechaResponso">fecha responso:</label>
+                  <input type="date" name="fechaResponso" />
+                </div>
+                <div className="inputContainer">
+                  <label htmlFor="lugarCementerio">lugar cementerio:</label>
+                  <input
+                    list="cementerios"
+                    type="text"
+                    name="otrolugarCementerio"
+                  />
+                  <datalist id="cementerios">
+                    <option value="Cementerio Parque Angol">
+                      Cementerio Parque Angol
+                    </option>
+                    <option value="Cementerio Parroquial Angol">
+                      Cementerio Parroquial Angol
+                    </option>
+                    <option value="Cementerio municipal Angol">
+                      Cementerio Municipal Angol
+                    </option>
+                  </datalist>
+                </div>
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button
+                type="button"
+                class="btn btn-secondary"
+                data-bs-dismiss="modal"
+              >
+                Cerrar
+              </button>
+              <button type="button" class="btn btn-primary btnAgregar">
+                Agregar
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <main className="managerPageMainContainer">
         <h1>Manager de Obituario</h1>
         <div className="addObituarioContainer">
-          <button className="btnAddObituario">
+          <button
+            className="btnAddObituario"
+            data-bs-toggle="modal"
+            data-bs-target="#modalAgregarObituario"
+          >
             Agregar al obituario{iconCross}
           </button>
         </div>
