f66b235032978a14b3e0314b25c6a74ec0fcb5c8
Andres Echeverria <andres@brandpro.cl>
2023-01-20 20:30:41 -0300

    change port

--- server/server.js
+++ server/server.js
@@ -3,7 +3,7 @@ const path = require("path");
 const app = express();
 const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 8080;
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
