4345982cb3dce7d11ff1572cfe87f32776d614ab
Andres Echeverria <andres@brandpro.cl>
2023-06-29 15:53:05 -0400

    build gtag

--- fcr_web/src/components/HomePage/HomePage.js
+++ fcr_web/src/components/HomePage/HomePage.js
@@ -87,14 +87,14 @@ function HomePage() {
             />
           </div>
           <div className="contactPhonesContainer">
-            <h4>
+            <h3 id="contactPhonesTitle--desktop">
               Contáctanos 24/7 a nuestros teléfonos <br /> o escríbanos a
               nuestro Whatsapp.
-            </h4>
-            <h4 id="contactPhonesTitle--mobile">
+            </h3>
+            <h3 id="contactPhonesTitle--mobile">
               Contáctanos 24/7 a nuestros teléfonos o escríbanos a nuestro
               Whatsapp.
-            </h4>
+            </h3>
             <div className="phonesWrapper">
               <a className="phoneButton" href={`tel:${fono1}`}>
                 <div className="callPhone">
@@ -111,7 +111,7 @@ function HomePage() {
                       d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
                     />
                   </svg>
-                  <h5>Llamar</h5>
+                  <h4 className="contactPhoneCall">Llamar</h4>
                 </div>
                 <h5>{fono1}</h5>
               </a>
@@ -130,7 +130,7 @@ function HomePage() {
                       d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.678.678 0 0 0 .178.643l2.457 2.457a.678.678 0 0 0 .644.178l2.189-.547a1.745 1.745 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.634 18.634 0 0 1-7.01-4.42 18.634 18.634 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877L1.885.511z"
                     />
                   </svg>
-                  <h5>Llamar</h5>
+                  <h4 className="contactPhoneCall">Llamar</h4>
                 </div>
                 <h5>{fono2}</h5>
               </a>
